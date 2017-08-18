9883aa0254fd22d5044094d58d27f6236b7a7076
Pradipta Sarker <techshuvo@gmail.com>
2017-08-18 15:43:48 +0600

    origin

---
+++ 404.php
@@ -0,0 +1,71 @@
+<?php include 'header.php' ?>
+
+	<div class="contentsection contemplete clear">
+		<div class="maincontent clear">
+			<div class="about">
+				<div class="notfound">
+    				<p><span>404</span> Not Found</p>
+    			</div>
+	        </div>
+		</div>
+		<div class="sidebar clear">
+			<div class="samesidebar clear">
+				<h2>Latest articles</h2>
+					<ul>
+						<li><a href="#">Category One</a></li>
+						<li><a href="#">Category Two</a></li>
+						<li><a href="#">Category Three</a></li>
+						<li><a href="#">Category Four</a></li>
+						<li><a href="#">Category Five</a></li>						
+					</ul>
+			</div>
+			<div class="samesidebar clear">
+				<h2>Popular articles</h2>
+					<div class="popular clear">
+						<h3><a href="#">Post title will be go here..</a></h3>
+						<a href="#"><img src="images/post1.jpg" alt="post image"/></a>
+						<p>Sidebar text will be go here.Sidebar text will be go here.Sidebar text will be go here.Sidebar text will be go here.</p>	
+					</div>
+					
+					<div class="popular clear">
+						<h3><a href="#">Post title will be go here..</a></h3>
+						<a href="#"><img src="images/post1.jpg" alt="post image"/></a>
+						<p>Sidebar text will be go here.Sidebar text will be go here.Sidebar text will be go here.Sidebar text will be go here.</p>	
+					</div>
+					
+					<div class="popular clear">
+						<h3><a href="#">Post title will be go here..</a></h3>
+						<a href="#"><img src="images/post1.jpg" alt="post image"/></a>
+						<p>Sidebar text will be go here.Sidebar text will be go here.Sidebar text will be go here.Sidebar text will be go here.</p>	
+					</div>
+					
+					<div class="popular clear">
+						<h3><a href="#">Post title will be go here..</a></h3>
+						<a href="#"><img src="images/post1.jpg" alt="post image"/></a>
+						<p>Sidebar text will be go here.Sidebar text will be go here.Sidebar text will be go here.Sidebar text will be go here.</p>	
+					</div>				
+				
+			</div>
+			
+		</div>
+	</div>
+
+	<div class="footersection templete clear">
+	  <div class="footermenu clear">
+		<ul>
+			<li><a href="#">Home</a></li>
+			<li><a href="#">About</a></li>
+			<li><a href="#">Contact</a></li>
+			<li><a href="#">Privacy</a></li>
+		</ul>
+	  </div>
+	  <p>&copy; Copyright Training with live project.</p>
+	</div>
+     <div class="fixedicon clear">
+		<a href="http://www.facebook.com"><img src="images/fb.png" alt="Facebook"/></a>
+		<a href="http://www.twitter.com"><img src="images/tw.png" alt="Twitter"/></a>
+		<a href="http://www.google.com"><img src="images/gl.png" alt="GooglePlus"/></a>
+		<a href="http://www.linkedin.com"><img src="images/in.png" alt="LinkedIn"/></a>
+	</div>
+</body>
+</html>
---
+++ post.php
@@ -0,0 +1,69 @@
+<?php include 'inc/header.php'; ?>
+
+	<div class="contentsection contemplete clear">
+		<div class="maincontent clear">
+			<div class="about">
+				<h2>Our post title here</h2>
+				<h4>April 10, 2016, 12:30 PM, By Delowar</h4>
+				<img src="images/post2.png" alt="MyImage"/>
+				<p>Our psot..Some text will be go here. Some text will be go here.Some text will be go here.Some text will be go here.Some text will be go here.Some text will be go here.Some text will be go here.Some text will be go here.Some text will be go here.Some text will be go here. Some text will be go here.Some text will be go here.Some text will be go here.Some text will be go here.Some text will be go here.Some text will be go here.Some text will be go here.Some text will be go here.</p>
+				
+				<p>About me..Some text will be go here. Some text will be go here.Some text will be go here.Some text will be go here.Some text will be go here.Some text will be go here.Some text will be go here.Some text will be go here.Some text will be go here.Some text will be go here. Some text will be go here.Some text will be go here.Some text will be go here.Some text will be go here.Some text will be go here.Some text will be go here.Some text will be go here.Some text will be go here.</p>
+
+				<p>About me..Some text will be go here. Some text will be go here.Some text will be go here.Some text will be go here.Some text will be go here.Some text will be go here.Some text will be go here.Some text will be go here.Some text will be go here.Some text will be go here. Some text will be go here.Some text will be go here.Some text will be go here.Some text will be go here.Some text will be go here.Some text will be go here.Some text will be go here.Some text will be go here.</p>
+				
+				<div class="relatedpost clear">
+					<h2>Related articles</h2>
+					<a href="#"><img src="images/post1.jpg" alt="post image"/></a>
+					<a href="#"><img src="images/post1.jpg" alt="post image"/></a>
+					<a href="#"><img src="images/post1.jpg" alt="post image"/></a>
+					<a href="#"><img src="images/post1.jpg" alt="post image"/></a>
+					<a href="#"><img src="images/post1.jpg" alt="post image"/></a>
+					<a href="#"><img src="images/post1.jpg" alt="post image"/></a>
+				</div>
+	</div>
+
+		</div>
+		<div class="sidebar clear">
+			<div class="samesidebar clear">
+				<h2>Latest articles</h2>
+					<ul>
+						<li><a href="#">Category One</a></li>
+						<li><a href="#">Category Two</a></li>
+						<li><a href="#">Category Three</a></li>
+						<li><a href="#">Category Four</a></li>
+						<li><a href="#">Category Five</a></li>						
+					</ul>
+			</div>
+			<div class="samesidebar clear">
+				<h2>Popular articles</h2>
+					<div class="popular clear">
+						<h3><a href="#">Post title will be go here..</a></h3>
+						<a href="#"><img src="images/post1.jpg" alt="post image"/></a>
+						<p>Sidebar text will be go here.Sidebar text will be go here.Sidebar text will be go here.Sidebar text will be go here.</p>	
+					</div>
+					
+					<div class="popular clear">
+						<h3><a href="#">Post title will be go here..</a></h3>
+						<a href="#"><img src="images/post1.jpg" alt="post image"/></a>
+						<p>Sidebar text will be go here.Sidebar text will be go here.Sidebar text will be go here.Sidebar text will be go here.</p>	
+					</div>
+					
+					<div class="popular clear">
+						<h3><a href="#">Post title will be go here..</a></h3>
+						<a href="#"><img src="images/post1.jpg" alt="post image"/></a>
+						<p>Sidebar text will be go here.Sidebar text will be go here.Sidebar text will be go here.Sidebar text will be go here.</p>	
+					</div>
+					
+					<div class="popular clear">
+						<h3><a href="#">Post title will be go here..</a></h3>
+						<a href="#"><img src="images/post1.jpg" alt="post image"/></a>
+						<p>Sidebar text will be go here.Sidebar text will be go here.Sidebar text will be go here.Sidebar text will be go here.</p>	
+					</div>				
+				
+			</div>
+			
+		</div>
+	</div>
+
+<?php include 'inc/footer.php'; ?>
